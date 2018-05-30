dd32851690c9579678a8a293e367deded2258495
srengasamy <srengasamy@thapovan-inc.com>
2018-05-30 13:52:07 +0530

    design updated

--- src/components/SearchOrion.vue
+++ src/components/SearchOrion.vue
@@ -18,13 +18,76 @@
                 <label for="failed">Failed</label>
             </div>
             <div class="form-group">
-                <input type="text" name="location" value="Location" />
-                <input type="text" name="ip" value="IP" />
-                <input type="text" name="date" value="Date" />
+                <input type="text" name="location" placeholder="Location"
+                       v-model="keyword" v-bind:class="{ error : isKeywordError}"
+                       @keyup.enter="search()" :readonly="isKeywordLoading"/>
+                <input type="text" name="ip" placeholder="IP"
+                       v-model="keyword" v-bind:class="{ error : isKeywordError}"
+                       @keyup.enter="search()" :readonly="isKeywordLoading"/>
+                <input type="text" name="date" placeholder="Date"
+                       v-model="keyword" v-bind:class="{ error : isKeywordError}"
+                       @keyup.enter="search()" :readonly="isKeywordLoading"/>
             </div>
         </div>
     </div>
 </template>
+
+<script>
+    import * as Spinner from 'vue-simple-spinner';
+    import Utils from '@/services/Utils';
+    import UserService from '@/services/UserService';
+    import { serverBus } from '../main';
+    //    import NotifyUtil from '@/services/NotifyUtil';
+
+    export default {
+        name: 'app-seach',
+        data: () => ({
+            keyword: '',
+            isKeywordError: false,
+            isKeywordLoading: false
+        }),
+        components: {
+            'vue-simple-spinner': Spinner
+        },
+
+        methods: {
+            search() {
+                if (this.keyword.trim().length === 0) {
+                    this.isKeywordError = true;
+                    return;
+                }
+                this.isKeywordError = false;
+                const isTraceID = Utils.validTraceId(this.keyword);
+                if (isTraceID) {
+                    this.isKeywordLoading = true;
+                    UserService.getInfoByTraceID(this.keyword)
+                        .then(resp => {
+                            this.isKeywordLoading = false;
+//                            if (!resp.data.status) {
+//                                this.$notify(NotifyUtil.toastError(resp.data.message));
+//                                return;
+//                            }
+                            const result = {
+                                users: resp.data.id,
+                                traceid: this.keyword,
+                            };
+                            serverBus.$emit('Charts', result);
+//                            this.$modal.show('modal-selectNick', result);
+                        }).catch(err => {
+                            this.isKeywordLoading = false;
+                        // this.$notify(NotifyUtil.toastError(err.response));
+                            if (err.response.status > 400 && err.response.status < 500) {
+                                this.$router.push({ path: '/' });
+                            }
+                        });
+                } else {
+                    const link = `/home/${this.keyword}`;
+                    this.$router.push({ path: link });
+                }
+            }
+        },
+    };
+</script>
 
 <style lang="scss" scoped>
     @import '../styles/components/search.scss';
